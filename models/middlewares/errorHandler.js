490bf9234e7f4f4a4f2581ac78ae74829490c4e3
Renato M. Santana <120746158+renatomsantana@users.noreply.github.com>
2024-12-18 10:32:32 -0300

    Create errorHandler.js

---
+++ models/middlewares/errorHandler.js
@@ -0,0 +1,5 @@
+// Middleware centralizado para tratamento de erros
+module.exports = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+};
