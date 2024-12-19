6a032acef5470266dcf7d2a4a5d59c730c03c6fe
Renato M. Santana <120746158+renatomsantana@users.noreply.github.com>
2024-12-19 15:17:32 -0300

    Update userRoutes.js

--- models/routes/userRoutes.js
+++ models/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.post('/', authMiddleware, userController.createUser);
+router.get('/', authMiddleware, userController.getAllUsers);
+router.get('/:id', authMiddleware, userController.getUserById);
+router.put('/:id', authMiddleware, userController.updateUser);
+router.delete('/:id', authMiddleware, userController.deleteUser);
 
 module.exports = router;
