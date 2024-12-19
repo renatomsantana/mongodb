fe935f6570606c79b93399a9d994a3bf051f5b51
Renato M. Santana <120746158+renatomsantana@users.noreply.github.com>
2024-12-19 15:17:10 -0300

    Create authMiddleware.js

---
+++ models/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'supersecretkey'; // Troque para variável de ambiente
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Access denied' });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Invalid token' });
+  }
+};
